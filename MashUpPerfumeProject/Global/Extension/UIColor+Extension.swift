c52c7bb5d5f22664e39593661fd9397359f0a3cc
HochanLee <1ghcks@naver.com>
2021-11-06 19:06:05 +0900

    [add] Color Extensions

---
+++ MashUpPerfumeProject/Global/Extension/UIColor+Extension.swift
@@ -0,0 +1,17 @@
+//
+//  UIColor+Extension.swift
+//  MashUpPerfumeProject
+//
+//  Created by Hochan Lee on 2021/11/06.
+//
+
+import UIKit
+
+extension UIColor {
+    static let black = UIColor(red: 27/255, green: 27/255, blue: 27/255, alpha: 1)
+    static let gray100 = UIColor(red: 199/255, green: 199/255, blue: 199/255, alpha: 1)
+    static let whiteGrey = UIColor(red: 248/255, green: 248/255, blue: 248/255, alpha: 1)
+    static let white = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1)
+    static let skyBlue = UIColor(red: 131/255, green: 218/255, blue: 255/255, alpha: 1)
+    static let violet = UIColor(red: 194/255, green: 183/255, blue: 228/255, alpha: 1)
+}
