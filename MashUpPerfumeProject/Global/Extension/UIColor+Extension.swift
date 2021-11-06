cec424c83f8f99cd552e955be7b112b3708ba368
HochanLee <1ghcks@naver.com>
2021-11-06 20:10:01 +0900

    [add] Color gray200

--- MashUpPerfumeProject/Global/Extension/UIColor+Extension.swift
+++ MashUpPerfumeProject/Global/Extension/UIColor+Extension.swift
@@ -10,6 +10,7 @@ import UIKit
 extension UIColor {
     static let black = UIColor(red: 27/255, green: 27/255, blue: 27/255, alpha: 1)
     static let gray100 = UIColor(red: 199/255, green: 199/255, blue: 199/255, alpha: 1)
+    static let gray200 = UIColor(red: 136/255, green: 136/255, blue: 136/255, alpha: 1)
     static let whiteGrey = UIColor(red: 248/255, green: 248/255, blue: 248/255, alpha: 1)
     static let white = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1)
     static let skyBlue = UIColor(red: 131/255, green: 218/255, blue: 255/255, alpha: 1)
