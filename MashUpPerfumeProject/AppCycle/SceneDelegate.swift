cf36c0a426bf75b41003a949ecbdc8c7ab9cd631
HochanLee <1ghcks@naver.com>
2021-11-06 20:51:24 +0900

    [fix] SceneDelegate rootViewController

--- MashUpPerfumeProject/AppCycle/SceneDelegate.swift
+++ MashUpPerfumeProject/AppCycle/SceneDelegate.swift
@@ -18,8 +18,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
-        let viewController = PerfumeDetailViewController()
-//        viewController.reactor = NewReactor()
+        let viewController = ViewController()
+        viewController.reactor = NewReactor()
         window?.rootViewController = viewController
         window?.makeKeyAndVisible()
     }
