e1fbba4550b5ec7a707f8720b4e2e4c0be1a47ad
HochanLee <1ghcks@naver.com>
2021-11-06 19:50:01 +0900

    [set] PerfumeDetailNameView layout

--- MashUpPerfumeProject/Flow/Detail/PerfumeDetailViewController.swift
+++ MashUpPerfumeProject/Flow/Detail/PerfumeDetailViewController.swift
@@ -10,12 +10,44 @@ import RxSwift
 import ReactorKit
 
 final class PerfumeDetailViewController: BaseViewController {
+    private let scrollView = UIScrollView()
     
+    private let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        
+        return stackView
+    }()
     
     private let perfumeImageView = UIImageView()
     
+    private let detailNameView = PerfumeDetailNameView()
+    
 //    private let
     var disposeBag = DisposeBag()
+    
+    override func setLayout() {
+        super.setLayout()
+        view.addSubviews(scrollView)
+        scrollView.addSubviews(stackView)
+        stackView.addArrangedSubviews(perfumeImageView, detailNameView)
+        
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            
+            stackView.topAnchor.constraint(equalTo: scrollView.contentLayoutGuide.topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.trailingAnchor),
+            stackView.bottomAnchor.constraint(equalTo: scrollView.contentLayoutGuide.bottomAnchor),
+            stackView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            
+            perfumeImageView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            perfumeImageView.heightAnchor.constraint(equalTo: perfumeImageView.widthAnchor, multiplier: 306/355),
+        ])
+    }
 }
 
 extension NoteCollectionViewController {
--- MashUpPerfumeProject/Flow/Detail/SupportViews/PerfumeDetailNameView.swift
+++ MashUpPerfumeProject/Flow/Detail/SupportViews/PerfumeDetailNameView.swift
@@ -8,10 +8,78 @@
 import UIKit
 
 final class PerfumeDetailNameView: UIView {
-    private let titleLabel: UILabel = {
+    private let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        
+        return stackView
+    }()
+    
+    private let brandLabel: UILabel = {
        let label = UILabel()
-//        label.textColor = .
+        label.textColor = .gray100
+        label.font = .systemFont(ofSize: 14, weight: .medium)
+        label.text = "brandLabel"
         
         return label
     }()
+    
+    private let titleLabel: UILabel = {
+       let label = UILabel()
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 18, weight: .semibold)
+        label.text = "titleLabel"
+        
+        return label
+    }()
+    
+    private let ownCountLabel: UILabel = {
+       let label = UILabel()
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 12, weight: .bold)
+        label.text = "0명이 이 향수를 가지고 있어요."
+        
+        return label
+    }()
+    
+    private let diggingButton: UIButton = {
+        let button = UIButton()
+        button.layer.cornerRadius = 25
+        button.layer.borderColor = UIColor(white: 0, alpha: 0.25).cgColor
+        button.layer.borderWidth = 0.5
+        
+        return button
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        setLayout()
+    }
+    
+    private func setLayout() {
+        addSubviews(stackView, diggingButton)
+        stackView.addArrangedSubviews(brandLabel, titleLabel, ownCountLabel)
+
+        stackView.setCustomSpacing(8, after: brandLabel)
+        stackView.setCustomSpacing(12, after: titleLabel)
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            stackView.trailingAnchor.constraint(equalTo: diggingButton.leadingAnchor, constant: -20),
+            stackView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            
+            diggingButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
+            diggingButton.heightAnchor.constraint(equalToConstant: 50),
+            diggingButton.widthAnchor.constraint(equalTo: diggingButton.heightAnchor),
+            diggingButton.centerYAnchor.constraint(equalTo: titleLabel.centerYAnchor)
+        ])
+    }
 }
