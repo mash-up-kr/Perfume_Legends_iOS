b22601dd21891d8d405314782cdb6976e56df1ad
최원석 <cws653@naver.com>
2021-11-07 21:48:39 +0900

    [fix] pbxproj 충돌 해결

--- MashUpPerfumeProject/Global/Base/BaseViewController.swift
+++ MashUpPerfumeProject/Global/Base/BaseViewController.swift
@@ -12,7 +12,7 @@ class BaseViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = UIColor(red: 0.898, green: 0.898, blue: 0.898, alpha: 1)
+        view.backgroundColor = UIColor(named: "Whitegray")
         setLayout()
     }
     
