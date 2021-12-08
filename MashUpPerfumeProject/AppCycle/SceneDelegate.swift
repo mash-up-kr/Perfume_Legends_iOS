4a6619d804a404c836253461f06737b11db5b110
최원석 <cws653@naver.com>
2021-12-09 07:15:21 +0900

    [fix] rootview setting

--- MashUpPerfumeProject/AppCycle/SceneDelegate.swift
+++ MashUpPerfumeProject/AppCycle/SceneDelegate.swift
@@ -20,16 +20,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window = UIWindow(windowScene: scene)
 
         let idProviderType = "UUID"
-//        let idProviderUserId = UIDevice.current.identifierForVendor!.uuidString
-        UserDefaults.standard.set(nil, forKey: "accesstoken")
-        let idProviderUserId = "adlsknaslkdnalkqwqeytejlcsncalkacnlakrdsnakaslcknaknlslaslckasjaslkcnaslaslcsdklcadsnklcappnpqslc;asknnaslkcasnlkkcanclascnaapoxcnaspivadpvanpxamzxczxmkllanlknqepfqnywfiu"
+        let idProviderUserId = UIDevice.current.identifierForVendor!.uuidString
         let accesstoken = UserDefaults.standard.string(forKey: "accesstoken")
         print("\(idProviderType),\(idProviderUserId), \(accesstoken)")
 
-//        let signInViewController = SignInViewController()
-//        signInViewController.reactor = SignInReactor(idProviderType: idProviderType, idProviderUserId: idProviderUserId, accesstoken: accesstoken)
-//        let rootNavigationViewController = UINavigationController(rootViewController: signInViewController)
-//        self.window?.rootViewController = rootNavigationViewController
+        let signInViewController = SignInViewController()
+        signInViewController.reactor = SignInReactor(idProviderType: idProviderType, idProviderUserId: idProviderUserId, accesstoken: accesstoken)
+        let rootNavigationViewController = UINavigationController(rootViewController: signInViewController)
+        self.window?.rootViewController = rootNavigationViewController
+
         let vc = SearchViewController()
         vc.reactor = SearchReactor()
         self.window?.rootViewController = UINavigationController(rootViewController: vc)
